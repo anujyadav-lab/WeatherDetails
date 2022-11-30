320cf5f5dafe3337fc6ecb9cada380e921ef9cd8
Anuj <ay517328@gmail.com>
2022-11-30 10:42:48 +0530

    added own features

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,5 @@
 const cityName = document.getElementById('cityName');
 const submitBtn = document.getElementById('submitBtn');
-
 const city_name = document.getElementById('city_name');
 const temp_real_val = document.getElementById('temp_real_val');
 const temp_status = document.getElementById('temp_status');
